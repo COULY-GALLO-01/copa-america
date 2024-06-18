d3f368cb2f0c00da38b0db4b0dc7f2563eeb3e1e
PATRICIO GALLO DILLON <d47351717@alumnos.itr.edu.ar>
2024-06-18 16:35:50 -0300

    si

--- index.py
+++ index.py
@@ -1,25 +1,46 @@
 import csv
-     
 
-def escribir(datos, nombre_archivo):
-    with open(nombre_archivo, mode='w', newline='', encoding='utf-8') as archivo:
-        escritor = csv.writer(archivo)
-        escritor.writerows(datos)
+def leer_equipos(nombre_archivo):
+    equipos = {}
+    with open(nombre_archivo, mode='r', newline='', encoding='utf-8') as archivo:
+        lector = csv.reader(archivo)
+        for fila in lector:
+            grupo, equipo = fila[0], fila[1]
+            if grupo not in equipos:
+                equipos[grupo] = {}
+            equipos[grupo][equipo] = {'Puntos': 0, 'PartidosJugados': 0, 'Victorias': 0, 'Empates': 0, 'Derrotas': 0, 'GolesAFavor': 0, 'GolesEnContra': 0, 'DiferenciaDeGoles': 0}
+    return equipos
 
+def leer_partidos(nombre_archivo):
+    partidos = []
+    with open(nombre_archivo, mode='r', newline='', encoding='utf-8') as archivo:
+        lector = csv.reader(archivo)
+        for fila in lector:
+            partidos.append({'Match Number': int(fila[0]), 'Home Team': fila[1], 'Away Team': fila[2], 'Home Team Goals': 0, 'Away Team Goals': 0})
+    return partidos
 
-datos_plantilla = [
-    ['Nombre', 'Edad', 'Correo']
-]
+def actualizar_resultados(nombre_archivo, equipos, partidos):
+    with open(nombre_archivo, mode='r', newline='', encoding='utf-8') as archivo:
+        lector = csv.reader(archivo)
+        for fila in lector:
+            if len(fila) != 3:
+                print(f"Formato incorrecto en la fila: {fila}")
+                continue
+            try:
+                match_number = int(fila[0])
+                home_team_goals = int(fila[1])
+                away_team_goals = int(fila[2])
+            except ValueError:
+                print(f"Datos no válidos en la fila: {fila}")
+                continue
+            
+            for partido in partidos:
+                if partido['Match Number'] == match_number:
+                    partido['Home Team Goals'] = home_team_goals
+                    partido['Away Team Goals'] = away_team_goals
+                    actualizar_posiciones(equipos, partido)
+                    break
 
-datos = [
-['Nombre', 'Edad', 'Correo'],
-['Juan Pérez', '28', 'juan.perez@example.com'],
-['María López', '34', 'maria.lopez@example.com'],
-['Carlos García', '22', 'carlos.garcia@example.com']
-]
-
- 
-escribir(datos, 'archivo.csv')
-escribir(datos_plantilla, 'archivo.csv')
-
-
+def actualizar_posiciones(equipos, partido):
+    home_team = partido['Home Team']
+    away
